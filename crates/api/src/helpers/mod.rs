795a6c08ab43957dfa7cb81f50ecce4e9c19817e
Manolis Liolios <manos@mystenlabs.com>
2025-06-23 12:56:50 +0300

    Adds formatting api endpoint

--- crates/api/src/helpers/mod.rs
+++ crates/api/src/helpers/mod.rs
@@ -1,11 +1,11 @@
 mod sandboxed;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     fs::{create_dir, File},
-    io::Write,
+    io::{Read, Write},
     process::Command,
 };
 use tempfile::TempDir;
@@ -16,6 +16,9 @@ use crate::helpers::sandboxed::run_sandboxed;
 /// The default timeout for the build process in seconds.
 const DEFAULT_TIMEOUT: u64 = 20;
 
+/// The default timeout for the format process in seconds.
+const DEFAULT_FORMAT_TIMEOUT: u64 = 2;
+
 #[derive(Serialize, Deserialize, PartialEq, Eq)]
 pub enum BuildType {
     Build,
@@ -23,11 +26,17 @@ pub enum BuildType {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct BuildRequest {
+    #[serde(flatten)]
+    pub code: Code,
+    pub build_type: BuildType,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct Code {
     pub sources: HashMap<String, String>,
     pub tests: HashMap<String, String>,
     pub name: String,
-    pub build_type: BuildType,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -37,7 +46,11 @@ pub struct BuildResult {
 }
 
 impl Code {
-    pub async fn build(&self) -> Result<BuildResult> {
+    pub async fn build(&self, build_type: BuildType) -> Result<BuildResult> {
+        if self.number_of_files() > 1 {
+            bail!("Only one file is supported at the moment");
+        }
+
         let start = Instant::now();
         let temp_dir = self.create_temp_project()?;
         eprintln!("Time taken to create temp project: {:?}", start.elapsed());
@@ -48,7 +61,11 @@ impl Code {
             "sui",
             &vec![
                 "move",
-                if self.is_test() { "test" } else { "build" },
+                if build_type == BuildType::Test {
+                    "test"
+                } else {
+                    "build"
+                },
                 "--path",
                 temp_dir.path().join("temp").to_str().unwrap(),
                 "--skip-fetch-latest-git-deps",
@@ -69,8 +86,36 @@ impl Code {
         })
     }
 
-    fn is_test(&self) -> bool {
-        self.build_type == BuildType::Test
+    /// Endpoint to format the codebase.
+    pub async fn format(&self) -> Result<Code> {
+        if self.number_of_files() > 1 {
+            bail!("Only one file is supported at the moment");
+        }
+
+        let temp_dir = self.create_temp_project()?;
+
+        let prettier_config = temp_dir.path().join(".prettierrc");
+        let mut file = File::create(prettier_config.clone())?;
+        file.write_all(PRETTIER_DEFAULT_CONFIG.as_bytes())?;
+
+        run_sandboxed(
+            "prettier-move",
+            &vec![
+                "--config",
+                prettier_config.to_str().unwrap(),
+                "--write",
+                temp_dir
+                    .path()
+                    .join("temp")
+                    .join("sources")
+                    .to_str()
+                    .unwrap(),
+            ],
+            DEFAULT_FORMAT_TIMEOUT,
+        )
+        .await;
+
+        Ok(self.read_formatted_files(&temp_dir)?)
     }
 
     fn create_temp_project(&self) -> anyhow::Result<TempDir> {
@@ -89,9 +134,6 @@ impl Code {
         let mut toml_file = File::create(project_path.join("Move.toml"))?;
         toml_file.write_all(base_toml_file(&self.name).as_bytes())?;
 
-        // let mut lock_file = File::create(project_path.join("Move.lock"))?;
-        // lock_file.write_all(BASE_LOCK_FILE.as_bytes())?;
-
         for (name, source) in &self.sources {
             let mut move_file = File::create(sources_path.join(format!("{}.move", name)))?;
             move_file.write_all(source.as_bytes())?;
@@ -103,6 +145,38 @@ impl Code {
         }
 
         Ok(temp_dir)
+    }
+
+    fn read_formatted_files(&self, temp_dir: &TempDir) -> Result<Self> {
+        let sources_path = temp_dir.path().join("temp").join("sources");
+        let tests_path = temp_dir.path().join("temp").join("tests");
+
+        let mut sources = HashMap::new();
+        let mut tests = HashMap::new();
+
+        for (name, _) in &self.sources {
+            let mut move_file = File::open(sources_path.join(format!("{}.move", name)))?;
+            let mut source = String::new();
+            move_file.read_to_string(&mut source)?;
+            sources.insert(name.clone(), source);
+        }
+
+        for (name, _) in &self.tests {
+            let mut move_file = File::open(tests_path.join(format!("{}.move", name)))?;
+            let mut source = String::new();
+            move_file.read_to_string(&mut source)?;
+            tests.insert(name.clone(), source);
+        }
+
+        Ok(Self {
+            sources,
+            tests,
+            name: self.name.clone(),
+        })
+    }
+
+    pub fn number_of_files(&self) -> usize {
+        self.sources.len() + self.tests.len()
     }
 }
 
@@ -126,6 +200,16 @@ pub fn verify_git_installed() -> anyhow::Result<()> {
     }
 }
 
+pub fn verify_prettier_move_installed() -> anyhow::Result<()> {
+    let output = Command::new("prettier-move").arg("--version").output()?;
+
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(anyhow::anyhow!("prettier CLI not installed"))
+    }
+}
+
 fn base_toml_file(name: &str) -> String {
     format!(
         r#"[package]
@@ -139,3 +223,13 @@ edition = "2024.beta"
         "#
     )
 }
+
+const PRETTIER_DEFAULT_CONFIG: &str = r#"
+{
+	"printWidth": 100,
+	"tabWidth": 4,
+	"useModuleLabel": true,
+	"autoGroupImports": "module",
+	"plugins": ["@mysten/prettier-plugin-move"]
+}
+"#;
